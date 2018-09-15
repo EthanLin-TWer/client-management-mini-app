76aef361b174d69c698630ba2556b18b30a5e07b
Linesh <linesh.simpcity@gmail.com>
2018-09-15 13:07:54 +0800

    :chore introduce eslint and prettier

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,13 +3,13 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    logs: [],
   },
-  onLoad: function () {
+  onLoad: function() {
     this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
+      logs: (wx.getStorageSync('logs') || []).map((log) => {
         return util.formatTime(new Date(log))
-      })
+      }),
     })
-  }
+  },
 })
