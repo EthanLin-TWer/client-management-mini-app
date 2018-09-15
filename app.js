69d985d48ac8c1fd7681ce7f59a41a27b06c1f04
Linesh <linesh.simpcity@gmail.com>
2018-09-15 12:55:47 +0800

    📝todo: add todolist

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ App({
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
+              // 但是这个 callback 是外界直接塞进来的，这样鬼知道整个应用有哪些地方往 app 对象塞值了，违反开闭原则啊
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
               }
